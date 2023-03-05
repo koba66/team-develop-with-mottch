5f6e341f24142da008d4ae62d1796c2e2819006d
mochi2434-toko <green.nbe1@gmail.com>
2023-03-05 23:55:01 +0900

    従業員テーブル　カラム追加

--- employees_management/db/employees_create.sql
+++ employees_management/db/employees_create.sql
@@ -1,1 +1,8 @@
 create table employee()
+alter table employee 
+add employee_id char(6) not null,
+add column employee_name varchar(10) not null,
+add column employee_name_kana varchar(20) not null,
+add column employee_division varchar(10),
+add column employee_email varchar(30) not null;
+alter table employee add primary key(employee_id);
