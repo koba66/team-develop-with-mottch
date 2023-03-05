766f8cb86825b98b2b0624fae84f8f215c01c7e1
mochi2434-toko <green.nbe1@gmail.com>
2023-03-05 23:44:34 +0900

    部署テーブル　カラム追加

--- employees_management/db/division_create.sql
+++ employees_management/db/division_create.sql
@@ -1,1 +1,4 @@
 create table division()
+alter table division add column division_code char(6) not null;
+alter table division add column division_name varchar(10) not null;
+alter table division add primary key(division_code);
