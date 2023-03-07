7205272b0359ce4e56371277af6c7bf853420a2e
mochi2434-toko <green.nbe1@gmail.com>
2023-03-07 22:01:59 +0900

    テーブル作成修正

--- employees_management/db/employees_create.sql
+++ employees_management/db/employees_create.sql
@@ -1,8 +1,7 @@
-create table employee()
-alter table employee 
-add employee_id char(6) not null,
-add column employee_name varchar(10) not null,
-add column employee_name_kana varchar(20) not null,
-add column employee_division varchar(10),
-add column employee_email varchar(30) not null;
-alter table employee add primary key(employee_id);
+create table employee(employee_id char(6) not null,
+						employee_name varchar(10) not null,
+						employee_name_kana varchar(20) not null,
+						employee_division varchar(10),
+						employee_email varchar(30) not null,
+						primary key(employee_id));
+						
